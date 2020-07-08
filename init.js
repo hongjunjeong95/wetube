e009dcc042b455fb1584303b6f13780bec2142d5
zpskek <wjdghdwns@gmail.com>
2020-07-08 18:04:30 +0900

    routes for global, user, video

---
+++ init.js
@@ -0,0 +1,12 @@
+import app from "./app";
+
+const PORT = 5000;
+
+// const handleListening = () =>
+//   console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+// app.listen(PORT, handleListening);
+const handleListening = () =>
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
