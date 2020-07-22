7a7a8629aedc1f4634813cd072247090c6533e9d
zpskek <wjdghdwns@gmail.com>
2020-07-22 18:56:14 +0900

    Mte Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,7 +1,10 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
+// play()와 pause()는 Read Only이기 때문에 boolean 값을 할당하지
+// 않고 단순히 메소드를 사용한다.
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -12,8 +15,21 @@ function handlePlayClick() {
   }
 }
 
+// 반면에 muted는 Read Only가 아니기 때문에
+//  = false or = true로 값을 할당한다.
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
