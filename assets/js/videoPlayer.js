92b94798aea59694355f8250a14b613fb5af054a
zpskek <wjdghdwns@gmail.com>
2020-07-23 10:51:52 +0900

    Browser compatibility for fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -6,7 +6,7 @@ const fullScrnBtn = document.getElementById("jsFullScreen");
 
 // play()와 pause()는 Read Only이기 때문에 boolean 값을 할당하지
 // 않고 단순히 메소드를 사용한다.
-function handlePlayClick() {
+const handlePlayClick = () => {
   if (videoPlayer.paused) {
     videoPlayer.play();
     playBtn.innerHTML = '<i class="fas fa-pause"></i>';
@@ -14,11 +14,11 @@ function handlePlayClick() {
     videoPlayer.pause();
     playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
-}
+};
 
 // 반면에 muted는 Read Only가 아니기 때문에
 //  = false or = true로 값을 할당한다.
-function handleVolumeClick() {
+const handleVolumeClick = () => {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
     volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
@@ -26,27 +26,43 @@ function handleVolumeClick() {
     videoPlayer.muted = true;
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
-}
+};
 
-function goFullScreen() {
-  videoContainer.requestFullscreen();
+const goFullScreen = () => {
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
   fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScrnBtn.removeEventListener("click", goFullScreen);
   fullScrnBtn.addEventListener("click", exitFullScreen);
-}
+};
 
-function exitFullScreen() {
-  document.exitFullscreen();
+const exitFullScreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
   fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScrnBtn.removeEventListener("click", exitFullScreen);
   fullScrnBtn.addEventListener("click", goFullScreen);
-}
+};
 
-function init() {
+const init = () => {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
-}
+};
 
 if (videoContainer) {
   init();
