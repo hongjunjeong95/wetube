cc9e28a3eb9255fac8be6628c9d1ef03a6791d0a
zpskek <wjdghdwns@gmail.com>
2020-07-27 14:10:39 +0900

    Multer uploads to AWS S3

--- middlewares.js
+++ middlewares.js
@@ -1,15 +1,38 @@
 // multer is primarily used for uploading files.
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvatar = multer({ dest: "uploads/avatars/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+});
+
+// multer에서 storage를 설정할 수 있다. default는 nodejs다.
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-hongjun/video",
+  }),
+});
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-hongjun/avatar",
+  }),
+});
+
+export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  console.log(req.loggedUser);
   next();
 };
 
@@ -28,6 +51,3 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home);
   }
 };
-
-export const uploadVideo = multerVideo.single("videoFile");
-export const uploadAvatar = multerAvatar.single("avatar");
